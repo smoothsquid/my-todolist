bacfcdceb895a085bb37f5fe6fd4702f4087f09c
HJ LEE <freejak5520@gmail.com>
2020-06-15 18:28:21 +0900

    할일 수정 Form 작성

--- task/forms.py
+++ task/forms.py
@@ -1,14 +1,25 @@
-from django.forms import ModelForm, TextInput
+from django import forms
 from task.models import Task
+from django.utils.translation import ugettext_lazy as _
 
-class SimpleTaskForm(ModelForm):
+class SimpleTaskForm(forms.ModelForm):
 
     class Meta:
         model = Task
         fields = ('title',)
         widgets = {
-            'title': TextInput(attrs={'placeholder': '할일'})
+            'title': forms.TextInput(attrs={'placeholder': _('To Do')})
         }
         labels = {
             'title': ''
+        }
+
+class ChangeTaskForm(forms.ModelForm):
+    
+    class Meta:
+        model = Task
+        fields = ("title", "text",)
+        labels = {
+            'title': _('Title'),
+            'text': _('Memo'),
         }
