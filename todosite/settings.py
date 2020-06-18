80cfaeebf99775aff5efc712c65f1236c26ba403
Jake Lee <freejak5520@gmail.com>
2020-06-18 15:07:29 +0900

    ALLOWED_HOSTS 오탈자 수정
    수정: 172.0.0.1 -> 127.0.0.1

--- todosite/settings.py
+++ todosite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$rirwys3^z@v*vusp@u^!=lfz(an%*&awl+8_3w_iz^kk)an^*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '172.0.0.1', ':0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', ':0']
 
 
 # Application definition
